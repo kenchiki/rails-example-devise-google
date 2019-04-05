804cd4b9ba5dc9aeba216e3723a3153d522677b0
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-04-05 19:17:44 +0900

    [add]rails setting

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,11 @@ module RailsExampleDeviseGoogle
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.generators do |g|
+      g.jbuilder false
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
