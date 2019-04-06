b98a8e8ffb47e4267f2b941de9f00da17cbb694d
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-04-06 18:28:54 +0900

    [update]リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,7 @@ class User < ApplicationRecord
          omniauth_providers: %i[google_oauth2]
 
   def self.find_or_create_user!(auth)
-    user = find_by(provider: auth.provider, uid: auth.uid) || build_user_by_auth(auth)
-    user.save!
-    user
-  end
-
-  def self.build_user_by_auth(auth)
-    User.new do |user|
+    find_or_create_by!(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.provider = auth.provider
